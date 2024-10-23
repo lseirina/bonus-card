c69f7614f0b6caefb8800c3f9e328509cbbb4770
root <lseirina87@gmail.com>
2024-10-23 10:41:51 +0300

    Added auth-token

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'rest-framework',
-    'drf-spectacular',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
