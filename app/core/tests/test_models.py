b64dfa1fff4490061bc01928a48de73d158af6d2
root <lseirina87@gmail.com>
2024-10-23 12:30:48 +0300

    Added test_creat_bonus_card

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,15 +1,26 @@
 """Tests for models."""
 from django.utils import timezone
 from django.test import TestCase
+
 from core.models import BonusCard
+
+from datetime import datetime
 
 
 class ModelTests(TestCase):
     """Test models."""
 
-    bonus_card = BonusCard.objects.create(
-        seria='sdfgh',
-        number='123ghjk',
-        issue_date=timezone.now(),
-        expiration_date=
-    )
+    def test_create_bonus_card(self):
+        bonus_card = BonusCard.objects.create(
+            seria='sdfgh',
+            number='123ghjk',
+            issue_date=timezone.now(),
+            expiration_date=datetime(2024, 12, 20, 12, 30),
+            balance=100.00,
+        )
+
+        self.asserIsNotNone(bonus_card.id)
+        self.assertEqual(bonus_card.seria, 'sdfgh')
+        self.assertEqual(bonus_card.number, '123ghjk')
+
+        
