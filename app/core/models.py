33a2b78f6418f33235111333c6e8037c61094e00
root <lseirina87@gmail.com>
2024-10-24 10:26:28 +0300

    Added test_check_expiration

--- app/core/models.py
+++ app/core/models.py
@@ -3,29 +3,29 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class BonusCard(models.Model):
     """BonusCard object."""
     status_choices = [
         ('inactive', 'Не активирована'),
         ('active', 'Активирована'),
-        ('expired', 'Просрочена')
+        ('expired', 'Просрочена'),
     ]
-    seria = models.CharField(max_length=10)
+
+    series = models.CharField(max_length=10)
     number = models.CharField(max_length=16, unique=True)
-    issue_date = models.DateTimeField(timezone.now())
+    issue_date = models.DateTimeField(default=timezone.now)
     expiration_date = models.DateTimeField()
     last_used_date = models.DateTimeField(null=True, blank=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.CharField(
-        max_length=10, choice=status_choices, default='inactive'
+        max_length=10, choices=status_choices, default='inactive'
         )
 
     def check_expiration(self):
         """Check if card is expired."""
-        if timezone.now > self.expiration_date:
+        if timezone.now() > self.expiration_date:
             self.status = 'expired'
             self.save()
 
     def __str__(self):
-        return f'{self.seria} {self.number} - {self.status}'
+        return f'{self.series} {self.number} - {self.status}'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,16 +11,31 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_bonus_card(self):
+        """Test create bonus card object."""
         bonus_card = BonusCard.objects.create(
-            seria='sdfgh',
+            series='sdfgh',
             number='123ghjk',
             issue_date=timezone.now(),
             expiration_date=datetime(2024, 12, 20, 12, 30),
             balance=100.00,
         )
 
-        self.asserIsNotNone(bonus_card.id)
-        self.assertEqual(bonus_card.seria, 'sdfgh')
+        self.assertIsNotNone(bonus_card.id)
+        self.assertEqual(bonus_card.series, 'sdfgh')
         self.assertEqual(bonus_card.number, '123ghjk')
 
-        
+    def test_check_expiration(self):
+        """Test checking expiration date."""
+
+        bonus_card = BonusCard.objects.create(
+            series='sdfgh',
+            number='123ghjk',
+            issue_date=timezone.now(),
+            expiration_date=timezone.make_aware(datetime(2023, 7, 20, 12, 30)),
+            balance=100.00,
+            status='active',
+        )
+        bonus_card.check_expiration()
+
+        bonus_card.refresh_from_db()
+        self.assertEqual(bonus_card.status, 'expired')
