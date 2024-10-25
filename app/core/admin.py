62e23d5ae45aacd5df7b12f986b4f74ac9e679b8
root <lseirina87@gmail.com>
2024-10-25 14:09:18 +0300

    Added BonusCardAdmin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,13 @@
+"""Django admin customization."""
 from django.contrib import admin
 
 from core import models
 
-admin.site.register(models.BonusCard)
+
+class BonusCardAdmin(admin.ModelAdmin):
+    """Define pages for bonuscasd admin."""
+    search_fields = ['series', 'number', 'status']
+    list_filter = ['status']
+
+
+admin.site.register(models.BonusCard, BonusCardAdmin)
