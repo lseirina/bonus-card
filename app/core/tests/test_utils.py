d41b265c51856de8cecd83e0f5ce82ee698f23b8
root <lseirina87@gmail.com>
2024-10-26 20:42:08 +0300

    Added testgenerate_crds correct expiration date

--- app/core/tests/test_utils.py
+++ app/core/tests/test_utils.py
@@ -10,7 +10,7 @@ from django.utils import timezone
 
 
 class GenerateCardsTest(TestCase):
-    def test_geberate_bonus_card_correct_count(self):
+    def test_geberate_cards_correct_count(self):
         """Test genereta correct count ob bonus cards."""
         series = 'TEST',
         count = 5
@@ -21,3 +21,20 @@ class GenerateCardsTest(TestCase):
         cards = BonusCard.objects.filter(series=series)
 
         self.assertEqual(cards.count(), count)
+
+    def test_generate_cards_correct_expiration_period(self):
+        """Test generate card correct expiration date."""
+        series = 'TEST'
+        count = 6
+        expiration_period = '1_month'
+        expected_expiration_date = timezone.now() + timedelta(days=30)
+
+        generate_cards(series, count, expiration_period)
+        cards = BonusCard.objects.filter(series=series)
+
+        for card in cards:
+            self.assertAlmostEqual(
+                card.expiration_date,
+                expected_expiration_date,
+                delta=timedelta(seconds=1)
+                )
