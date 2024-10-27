1963923ea17768cb08c42805fb5137260812f113
root <lseirina87@gmail.com>
2024-10-27 23:24:15 +0300

    Added tokenauthentication to views

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,8 @@
 """Views for Bonus card API."""
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+
 from core.models import BonusCard
 from core.serializers import BonusCardSerializer
 
@@ -8,6 +11,8 @@ class BonusCardViewSet(viewsets.ModelViewSet):
     """View for bonus cards."""
     queryset = BonusCard.objects.all()
     serializer = BonusCardSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def perform_update(self, serializer):
         """Check the expiration date when update."""
