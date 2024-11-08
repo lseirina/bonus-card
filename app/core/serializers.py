6cdf3b8633ff7d4fa5ec38f2374012be0b688836
root <lseirina87@gmail.com>
2024-11-08 14:02:13 +0300

    Added UserSerializer

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -2,12 +2,29 @@
 Serializers for BonusCard API.
 """
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth.models import User
 
 from core.models import BonusCard
 
 
+class UserSerializer(serializers.ModelSerialiser):
+    """Serializer for user object."""
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def creat(self, validated_data):
+            """Create and return user with token."""
+            user = User.objects.create_user(**validated_data)
+            Token.objects.create(user=user)
+            return user
+
+
 class BonusCardSerializer(serializers.ModelSerializer):
-    """Serializer fot bonus cards."""
+    """Serializer for bonus cards."""
     class Meta:
         model = BonusCard
         fields = [
