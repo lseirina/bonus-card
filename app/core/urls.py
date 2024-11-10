ef1e8796875bab25bb04606b9a61fcc18f047c2c
root <lseirina87@gmail.com>
2024-11-10 19:27:04 +0300

    Added register/ yrl

--- app/core/urls.py
+++ app/core/urls.py
@@ -4,7 +4,10 @@ from django.urls import (
     include
     )
 from rest_framework.routers import DefaultRouter
-from core.views import BonusCardViewSet
+from core.views import (
+    BonusCardViewSet,
+    UserRegistrationView,
+    )
 
 router = DefaultRouter()
 router.register('cards', BonusCardViewSet, basename='card')
@@ -12,5 +15,6 @@ router.register('cards', BonusCardViewSet, basename='card')
 app_name = 'card'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='register')
 ]
